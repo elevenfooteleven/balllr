36e40426c679c1435d12d0979d7a3b09efa3f290
Chris LeBlanc <spacesuitdiver@gmail.com>
2017-11-14 13:43:32 -0600

    Move catch in on promise chain.

--- api/Dribbble.js
+++ api/Dribbble.js
@@ -4,15 +4,16 @@ const { accessToken } = require('../secret');
 export const fetchShotsAndComments = () =>
   fetch(`${apiEndpoint}?page=1&timeframe=month&per_page=50&access_token=${accessToken}`)
     .then(response => response.json())
-    .then(shots => {
-      return Promise.all(shots.map(shot =>
+    .then(shots =>
+      Promise.all(shots.map(shot =>
         fetch(`${shot.comments_url}?access_token=${accessToken}`)
           .then(response => response.json())
           .then(comments => ({
             ...shot,
             comments,
           }))
-      ))
+        ))
+    )
     .catch(error => {
       console.log(error);
       Alert.alert(
@@ -20,4 +21,3 @@ export const fetchShotsAndComments = () =>
         'Looks like we hit Dribbble’s API rate limit. Load this app again in 1000ms.',
       );
     });
-  });
