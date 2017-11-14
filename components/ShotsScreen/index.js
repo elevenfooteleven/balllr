4e97aa2bf62416d4bd8799022ba296e674264e30
Zachary Gibson <zackgib@aol.com>
2017-11-14 14:29:13 -0600

    Adds extra points for parts

--- components/ShotsScreen/index.js
+++ components/ShotsScreen/index.js
@@ -1,3 +1,11 @@
-import Part1 from './Part1';
+// import Part1 from './Part1';
+// export default Part1;
+//
+// import Part2 from './Part2';
+// export default Part2;
+//
+// import Part3 from './Part3';
+// export default Part3;
 
-export default Part1;
+import Part4 from './Part4';
+export default Part4;
